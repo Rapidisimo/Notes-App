bd1fa3cae2d6573ccaf196747c92c1960e1cd506
Josué Rodriguez <13837690+Rapidisimo@users.noreply.github.com>
2023-07-18 14:15:01 -0400

    added firebase

---
+++ firebase.js
@@ -0,0 +1,15 @@
+import { initializeApp } from "firebase/app";
+import { collection, getFirestore } from "firebase/firestore"
+
+const firebaseConfig = {
+  apiKey: "AIzaSyA9WajWZEZW9B33zJCtPgB0dn9q9jCbKNI",
+  authDomain: "react-notes-7d16a.firebaseapp.com",
+  projectId: "react-notes-7d16a",
+  storageBucket: "react-notes-7d16a.appspot.com",
+  messagingSenderId: "834643181523",
+  appId: "1:834643181523:web:092ed1d607b1938914b926"
+};
+
+const app = initializeApp(firebaseConfig);
+const db = getFirestore(app)
+const notesCollection = collection(db, "notes")
